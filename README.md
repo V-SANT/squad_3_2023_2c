511ea22081a5bfacb9bf68e335d67b11b6f7c516
Manuel <jpol@fi.uba.ar>
2023-11-28 15:44:04 -0300

    se persisten los datos, chau tests cucumber anda a cagar

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # squad_3_2023_2c
+
+link para levantar la api: http://localhost:8080/swagger-ui.html#/memo-1-tpg
+
+contrasenia de notion database: 8JGIpKWOA7Fs
--- src/main/java/com/aninfo/Memo1TPG.java
+++ src/main/java/com/aninfo/Memo1TPG.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 // import org.springframework.http.HttpStatus;
 // import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +24,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @SpringBootApplication
+@EnableJpaRepositories
 @EnableSwagger2
 
 public class Memo1TPG {
