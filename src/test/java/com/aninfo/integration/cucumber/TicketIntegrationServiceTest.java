cf4134c46dbba1bc6324a3ad4222fad40db44415
V-SANT <vsantander@fi.uba.ar>
2023-12-02 19:09:48 -0300

    Fix campos incompletos

--- src/test/java/com/aninfo/integration/cucumber/TicketIntegrationServiceTest.java
+++ src/test/java/com/aninfo/integration/cucumber/TicketIntegrationServiceTest.java
@@ -23,41 +23,41 @@ public class TicketIntegrationServiceTest {
     TicketService ticketService;
 
     Ticket createTicket(String title) {
-        return ticketService.createTicket(title, "", Status.NOT_STARTED, Severity.S1, Priority.LOW, "", "", 0L, new ArrayList<>() ,LocalDate.now(), LocalDate.now());
+        return ticketService.createTicket(title, "", Status.NOT_STARTED, Severity.S1, Priority.LOW, "", "", 0L, 0L,new ArrayList<>() ,LocalDate.now(), LocalDate.now());
     }
 
     Ticket createTicket(){
-        return ticketService.createTicket("", "", Status.NOT_STARTED, Severity.S1, Priority.LOW, "", "", 0L, new ArrayList<>() ,LocalDate.now(), LocalDate.now());
+        return ticketService.createTicket("", "", Status.NOT_STARTED, Severity.S1, Priority.LOW, "", "", 0L, 0L,new ArrayList<>() ,LocalDate.now(), LocalDate.now());
     }
 
     Ticket updateTicketName(Long code, String new_name){
         Ticket ticket = ticketService.findByCode(code);
-        return ticketService.updateTicket(code,new_name, ticket.getDescription(), ticket.getStatus(), ticket.getSeverity(), ticket.getPriority(), ticket.getClosingDate());
+        return ticketService.updateTicket(code,new_name, ticket.getDescription(), ticket.getStatus(), ticket.getSeverity(), ticket.getPriority(), ticket.getProduct(),ticket.getVersion(),ticket.getClientId(),ticket.getAssignatedEmployeeId(),ticket.getAssociatedTasks(),ticket.getClosingDate());
     }
 
     Ticket updateTicketSeverity(Long code, Severity new_severity){
         Ticket ticket = ticketService.findByCode(code);
-        return ticketService.updateTicket(code,ticket.getTitle(), ticket.getDescription(), ticket.getStatus(), new_severity, ticket.getPriority(), ticket.getClosingDate());
+        return ticketService.updateTicket(code,ticket.getTitle(), ticket.getDescription(), ticket.getStatus(), new_severity, ticket.getPriority(),  ticket.getProduct(),ticket.getVersion(),ticket.getClientId(),ticket.getAssignatedEmployeeId(),ticket.getAssociatedTasks(),ticket.getClosingDate());
     }
 
     Ticket updateTicketPriority(Long code, Priority new_priority){
         Ticket ticket = ticketService.findByCode(code);
-        return ticketService.updateTicket(code,ticket.getTitle(), ticket.getDescription(), ticket.getStatus(), ticket.getSeverity(), new_priority, ticket.getClosingDate());
+        return ticketService.updateTicket(code,ticket.getTitle(), ticket.getDescription(), ticket.getStatus(), ticket.getSeverity(), new_priority, ticket.getProduct(),ticket.getVersion(),ticket.getClientId(),ticket.getAssignatedEmployeeId(),ticket.getAssociatedTasks(), ticket.getClosingDate());
     }
 
     Ticket updateTicketInfo(Long code, String new_info){
         Ticket ticket = ticketService.findByCode(code);
-        return ticketService.updateTicket(code,ticket.getTitle(), new_info, ticket.getStatus(), ticket.getSeverity(), ticket.getPriority(), ticket.getClosingDate());
+        return ticketService.updateTicket(code,ticket.getTitle(), new_info, ticket.getStatus(), ticket.getSeverity(), ticket.getPriority(),  ticket.getProduct(),ticket.getVersion(),ticket.getClientId(),ticket.getAssignatedEmployeeId(),ticket.getAssociatedTasks(),ticket.getClosingDate());
     }
 
     Ticket updateTicketStatus(Long code, Status new_status){
         Ticket ticket = ticketService.findByCode(code);
-        return ticketService.updateTicket(code, ticket.getTitle(), ticket.getDescription(), new_status, ticket.getSeverity(), ticket.getPriority(), ticket.getClosingDate());
+        return ticketService.updateTicket(code, ticket.getTitle(), ticket.getDescription(), new_status, ticket.getSeverity(), ticket.getPriority(),  ticket.getProduct(),ticket.getVersion(),ticket.getClientId(),ticket.getAssignatedEmployeeId(),ticket.getAssociatedTasks(),ticket.getClosingDate());
     }
 
     Ticket updateTicketEstimatedFinishDate(Long code, LocalDate new_date){
         Ticket ticket = ticketService.findByCode(code);
-        return ticketService.updateTicket(code,ticket.getTitle(), ticket.getDescription(), ticket.getStatus(), ticket.getSeverity(), ticket.getPriority(), new_date);
+        return ticketService.updateTicket(code,ticket.getTitle(), ticket.getDescription(), ticket.getStatus(), ticket.getSeverity(), ticket.getPriority(), ticket.getProduct(),ticket.getVersion(),ticket.getClientId(),ticket.getAssignatedEmployeeId(),ticket.getAssociatedTasks(), new_date);
     }
 
     public Ticket findByCode(Long code){
