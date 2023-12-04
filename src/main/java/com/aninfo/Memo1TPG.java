2c26cfcfcdab7af97960a951aa76e98b06cbc033
Manuel <jpol@fi.uba.ar>
2023-12-04 12:34:16 -0300

    a ver si asi ya no hay problemas con el CORS

--- src/main/java/com/aninfo/Memo1TPG.java
+++ src/main/java/com/aninfo/Memo1TPG.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -43,11 +44,12 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RestController
 @SpringBootApplication
 @EnableSwagger2
 @EnableJpaRepositories
+@Configuration
 public class Memo1TPG {
 
 	@Autowired
